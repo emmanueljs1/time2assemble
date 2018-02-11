75898bfbae83c8db49b20e4d752979479e01db47
Emmanuel Suarez <emsu@seas.upenn.edu>
2018-02-10 19:30:00 -0500

    updating login button so it requires a username to go to next screen, event dashboard shows a welcome label

--- time2assemble/time2assemble/EventDashboardViewController.swift
+++ time2assemble/time2assemble/EventDashboardViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class EventDashboardViewController: UIViewController {
     
-    @IBOutlet weak var username: UITextField!
+    var username : String!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Welcome " + username + "!"
+        label.sizeToFit()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
--- time2assemble/time2assemble/LoginViewController.swift
+++ time2assemble/time2assemble/LoginViewController.swift
@@ -11,16 +11,31 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var username: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func logInButtonClicked(_ sender: Any) {
+        if let usernameString = username.text {
+            if !usernameString.isEmpty {
+                performSegue(withIdentifier: "toEventDashboardView", sender: sender)
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let eventDashboardVC = segue.destination as? EventDashboardViewController {
+            eventDashboardVC.username = username.text
+        }
+    }
+    
+    
 }
 
