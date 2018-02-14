2c43b13d1a6a0fbac2a607b89d463cd65ede6a4d
Julia Chun <hjchun96@gmail.com>
2018-02-13 20:23:50 -0500

    Connected Facebook API - Info.plist, appdelegate, view changed

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '9.0'
 pod 'Firebase/Database'
+pod 'FBSDKLoginKit'
 
 target 'time2assemble' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
--- time2assemble/LoginViewController.swift
+++ time2assemble/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
     var ref: DatabaseReference!
@@ -18,6 +19,12 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         ref = Database.database().reference()
         self.ref.child("users").child("0").setValue(["username": usernameTextField.text])
+        
+        // Facebook Login
+        let loginButton = FBSDKLoginButton()
+//        let loginButton = FBSDKButton() // LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
