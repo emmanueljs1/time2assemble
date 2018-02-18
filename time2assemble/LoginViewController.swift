77e28d46d68df162d621eeed6238ed499a7f8c73
Hana Pearlman <hanap@sas.upenn.edu>
2018-02-18 00:26:30 -0500

    i got the wrong combination of everything

--- time2assemble/LoginViewController.swift
+++ time2assemble/LoginViewController.swift
@@ -10,22 +10,64 @@ import UIKit
 import Firebase
 import FBSDKLoginKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     var ref: DatabaseReference!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    var fbLoginSuccess = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         ref = Database.database().reference()
-//        self.ref.child("users").child("0").setValue(["username": usernameTextField.text])
+        if (FBSDKAccessToken.current() != nil)
+        {
+            //fbLoginSuccess = true
+        }
         
         // Facebook Login
         let loginButton = FBSDKLoginButton()
-//        let loginButton = FBSDKButton() // LoginButton(readPermissions: [ .publicProfile ])
         loginButton.center = view.center
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.delegate = self
         view.addSubview(loginButton)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if (FBSDKAccessToken.current() != nil && fbLoginSuccess == true)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            performSegue(withIdentifier: "toEventDashboard", sender: self)
+        }
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("here we are at the callback")
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            print("here we are with no error")
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                fbLoginSuccess = true
+                print("here we are time to segue")
+                performSegue(withIdentifier: "toEventDashboard", sender: self)
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        FBSDKAccessToken.current()
+        //handle logout
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,8 +94,13 @@ class LoginViewController: UIViewController {
     // MARK: Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("here we are preparing to segue")
         if let eventDashboard = segue.destination as? EventDashboardController {
-            eventDashboard.username = usernameTextField.text
+            if fbLoginSuccess {
+                self.show(eventDashboard, sender: self)
+                print("here we are!!! seguing!!!")
+                eventDashboard.username = usernameTextField.text
+            }
         }
     }
     
