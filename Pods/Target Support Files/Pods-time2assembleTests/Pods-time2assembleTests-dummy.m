da19306a761015aab1e1062376b55f957ea5dd8b
Emmanuel Suarez <emsu@seas.upenn.edu>
2018-03-31 16:56:49 -0400

    removed Pods from tracked files

--- Pods/Target Support Files/Pods-time2assembleTests/Pods-time2assembleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_time2assembleTests : NSObject
-@end
-@implementation PodsDummy_Pods_time2assembleTests
-@end
--- Pods/Target Support Files/Pods-time2assembleUITests/Pods-time2assembleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_time2assembleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_time2assembleUITests
-@end
