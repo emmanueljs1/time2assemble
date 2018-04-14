598ba8e5e2843b7a291c2704a290a055212de6e3
Yuan Xu <xuyuan@seas.upenn.edu>
2018-04-14 16:34:23 -0400

    intermediate changes -- in the process of creating a table view for notifications  -- only committing to be able to pull Hana's db controller function that I want

--- time2assemble/ViewControllers/NotificationsViewController.swift
+++ time2assemble/ViewControllers/NotificationsViewController.swift
@@ -8,17 +8,62 @@
 
 import UIKit
 
-class NotificationsViewController: UIViewController {
+class NotificationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var user : User!
+    var notifications : [EventNotification] = [];
+    @IBOutlet weak var notificationsTableView: UITableView!
+    var loaded = false
+    
+    func loadNotifications() {
+        FirebaseController.getNotificationsForUser(user.id, {
+            (notificationsList) in
+            self.notifications = notificationsList
+            self.loaded = true
+            self.notificationsTableView.reloadData()
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifications.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "notifications", for: indexPath)
+        
+        if loaded {
+            let notification = notifications[indexPath.row]
+            
+            switch notification.type {
+            case 1:
+            case 2:
+            case 3:
+            }
+            cell.textLabel!.text =
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        notificationsTableView.dataSource = self
+        notificationsTableView.delegate = self
+        notificationsTableView.separatorColor = UIColor.clear
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        notifications = []
+        loaded = false
+        loadNotifications()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
 
