e4abac762ecd86540e29bb0d54dd0a007d355618
Emmanuel Suarez <emsu@seas.upenn.edu>
2018-04-20 01:43:48 -0400

    fixing bug where filling availability for entire day doesn't work

--- time2assemble/ViewControllers/FillAvailViewController.swift
+++ time2assemble/ViewControllers/FillAvailViewController.swift
@@ -171,6 +171,9 @@ class FillAvailViewController: UIViewController {
             }
             i += 1
         }
+        if let start = startOpt {
+            ranges += [(start, event.noLaterThan)]
+        }
         userAvailabilities[formatter.string(from: currentDate)] = ranges
         currentDate = currentDate + TimeInterval(oneDay)
         currentDateLabel.text = displayFormatter.string(from: currentDate)
@@ -284,7 +287,7 @@ class FillAvailViewController: UIViewController {
         }
         else if !eventBeingCreated && currentDate > endDate! {
             Availabilities.setEventAvailabilitiesForUser(event.id, String(user.id), userAvailabilities)
-            performSegue(withIdentifier: "toDashboard", sender: self)
+            performSegue(withIdentifier: "toEventDetails", sender: self)
         }
         else {
             for aView in selectableViewsStackView.arrangedSubviews {
